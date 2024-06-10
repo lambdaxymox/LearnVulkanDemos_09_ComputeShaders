cdfbf2523413837b883aad31aaebb33ebf48f85b
LambdaXymox <lambda.xymox@gmail.com>
2024-06-09 21:20:11 -0500

    Initialize window.

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,47 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class App {
 public:
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        m_window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Engine", nullptr, nullptr);
+    }
+
     void initVulkan() {
 
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(m_window)) {
+            glfwPollEvents();
+        }
     }
 
     void cleanup() {
+        glfwDestroyWindow(m_window);
 
+        glfwTerminate();
     }
+
+    GLFWwindow* m_window;
 };
 
 int main() {
