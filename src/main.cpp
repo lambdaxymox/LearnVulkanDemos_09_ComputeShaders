1effc77ff7ab6831bc2428235952b21bf05a39c7
LambdaXymox <lambda.xymox@gmail.com>
2024-06-11 18:45:22 -0500

    Minor refactoring.

--- src/main.cpp
+++ src/main.cpp
@@ -87,52 +87,7 @@ std::vector<const char*> convertToCStrings(const std::vector<std::string>& strin
     
     return cStrings;
 }
-/*
-using MissingPhysicalDeviceRequirements = PhysicalDeviceRequirements;
 
-class PhysicalDeviceInfoOps {
-public:
-    static PhysicalDeviceProperties getAvailableVulkanDeviceExtensions(VkPhysicalDevice physicalDevice) {
-        auto deviceExtensionProperties =  std::vector<VkExtensionProperties> {};
-        uint32_t numInstanceExtensions = 0;
-        vkEnumerateDeviceExtensionProperties(physicalDevice, nullptr, &numInstanceExtensions, nullptr);
-        if (numInstanceExtensions > 0) {
-            deviceExtensionProperties.resize(numInstanceExtensions);
-            vkEnumerateDeviceExtensionProperties(
-                physicalDevice,
-                nullptr, 
-                &numInstanceExtensions, 
-                deviceExtensionProperties.data()
-            );
-        }
-
-        return PhysicalDeviceProperties(deviceExtensionProperties);
-    }
-
-    static MissingPhysicalDeviceRequirements detectMissingRequiredDeviceExtensions(
-        const PhysicalDeviceProperties& physicalDeviceProperties,
-        const PhysicalDeviceRequirements& physicalDeviceRequirements
-    ) {
-        auto missingExtensionNames = std::vector<std::string> {};
-        auto requiredExtensions = physicalDeviceRequirements.getExtensions();
-        for (const auto& requiredExtension : requiredExtensions) {
-            auto found = std::find_if(
-                std::begin(requiredExtensions),
-                std::end(requiredExtensions),
-                [requiredExtension](const auto& extension) {
-                    return strcmp(requiredExtension.data(), extension.data()) == 0;
-                }
-            );
-            auto extensionNotFound = (found == std::end(requiredExtensions));
-            if (extensionNotFound) {
-                missingExtensionNames.emplace_back(requiredExtension);
-            }
-        }
-
-        return PhysicalDeviceRequirements(missingExtensionNames);
-    }
-};
-*/
 struct QueueFamilyIndices final {
     std::optional<uint32_t> graphicsFamily;
     std::optional<uint32_t> presentFamily;
