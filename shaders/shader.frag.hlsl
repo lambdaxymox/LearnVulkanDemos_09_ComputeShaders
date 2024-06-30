165bd219685ea37b914f7adec029b86e2f4309f1
LambdaXymox <lambda.xymox@gmail.com>
2024-06-29 22:07:12 -0500

    Add HLSL shaders.

--- shaders/shader.frag.hlsl
+++ shaders/shader.frag.hlsl
@@ -1,18 +1,22 @@
+Texture2D<float4> texSampler : register(t1, space0);
+SamplerState texSamplerState : register(s1, space0);
+
 struct PS_Input {
     float4 position : SV_POSITION;
     float3 fragColor : TEXCOORD0;
+    float2 fragTexCoord: TEXCOORD1;
 };
 
 struct PS_Output {
-    float4 outColor : COLOR0;
+    float4 outColor : SV_TARGET0;
 };
 
 
-PS_Output main(PS_Input input) : SV_TARGET {    
-    float4 outFragColor = float4(input.fragColor, 1.0);
+PS_Output main(PS_Input input) {    
+    float4 outFragColor = texSampler.Sample(texSamplerState, input.fragTexCoord);
     
     PS_Output output;
-    output.outColor = float4(outFragColor);
+    output.outColor = outFragColor;
     
     return output;
 }
--- shaders/shader.vert.hlsl
+++ shaders/shader.vert.hlsl
@@ -1,11 +1,13 @@
 struct VS_Input {
     float2 position : TEXCOORD0;
     float3 color : TEXCOORD1;
+    float2 texCoord: TEXCOORD2;
 };
 
 struct VS_Output {
     float4 position : SV_POSITION;
     float3 fragColor : TEXCOORD0;
+    float2 fragTexCoord: TEXCOORD1;
 };
 
 struct VS_InputConstants {
@@ -22,10 +24,12 @@ cbuffer ubo : register(b0) {
 VS_Output main(VS_Input input) {
     float4 outPosition = mul(ubo.proj, mul(ubo.view, mul(ubo.model, float4(input.position, 0.0f, 1.0f))));
     float3 outFragColor = input.color;
+    float2 outFragTexCoord = input.texCoord;
     
     VS_Output output;
     output.position = outPosition;
     output.fragColor = outFragColor;
+    output.fragTexCoord = outFragTexCoord;
     
     return output;
 }
