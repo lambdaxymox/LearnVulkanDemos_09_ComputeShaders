d330a9f155c22c4ab7709aaa42fa5ad35d35d5aa
LambdaXymox <lambda.xymox@gmail.com>
2024-07-02 00:36:01 -0500

    Minor refactoring.

--- shaders/shader_compute.comp.glsl
+++ shaders/shader_compute.comp.glsl
@@ -20,8 +20,8 @@ layout(std140, binding = 2) buffer ParticleSSBOOut {
 
 layout (local_size_x = 256, local_size_y = 1, local_size_z = 1) in;
 
-void main() 
-{
+
+void main() {
     uint index = gl_GlobalInvocationID.x;  
 
     Particle particleIn = particlesIn[index];
@@ -36,5 +36,4 @@ void main()
     if ((particlesOut[index].position.y <= -1.0) || (particlesOut[index].position.y >= 1.0)) {
         particlesOut[index].velocity.y = -particlesOut[index].velocity.y;
     }
-
 }
